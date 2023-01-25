a48403079746ecf75f18c45f8d3c0c15cc98eb0e
Alona Harnyk <alyonagarnyk@gmail.com>
2023-01-25 11:41:25 +0200

    25.01

--- index.js
+++ index.js
@@ -1,125 +1,86 @@
-// const elem1 = document.querySelector('.text')
-// console.log(elem1)
+// const btn = document.querySelector('button')
 
-// const elem2 = document.querySelectorAll('li')
-// console.log(elem2)
+// const clickHandler = () => {
+//     console.log('CLICK!!!!')
+// }
 
-// const elem3 = document.querySelector('.item1')
-// // console.log(elem3)
+// btn.addEventListener('click', clickHandler)
 
-// const elem4 = elem3.querySelector('p')
-// console.log(elem4)
+// const items = document.querySelectorAll('.item')
 
-// console.log(elem3.firstElementChild)
+// const onClick = (event) => {
+// if(event.target.textContent === 'xrtxcx') {
+//     console.log('HERE')
+// }
+// }
 
-// elem4.textContent = 'Super text'
+// items.forEach(item => item.addEventListener('click', onClick))
 
-// console.log(elem4.textContent)
-
-// console.log(elem3.classList.contains('item1'))
-// console.log(elem3.classList.contains('superitem'))
-
-// elem3.classList.add('superitem')
-// console.log(elem3.classList.contains('superitem'))
-// elem3.classList.remove('item1')
-// elem3.classList.toggle('superitem')
-
-// elem2.forEach(elem => elem.classList.add('item'))
-// elem4.style.color = 'red'
-
-// const img = document.querySelector('img')
-// // img.setAttribute('alt', 'image')
-// img.alt = 'image'
-// console.log(img.getAttribute('src'))
-// console.log(img.attributes)
-// img.removeAttribute('src')
-
-// const btn = document.querySelector('[data-action="save"]')
-// console.log(btn.dataset.save)
-
-// const title = document.createElement('h1');
-// const text = document.createElement('p');
-
-// console.log(title)
-// title.textContent = 'Hello'
-// title.style.backgroundColor = 'yellow'
-
-// text.textContent = 'Test'
-
-// const body = document.body
-// console.log(body)
-// body.prepend(title, text)
-
-// text.remove()
-
-// const span = document.createElement('span')
-// span.textContent = '!!!'
-// title.append(span)
-
-// console.log(title.innerHTML)
-
-// title.innerHTML = 'Hello!!!'
-
-
-// const div = document.querySelector('.div')
-
-// div.innerHTML = '<p>1</p><p>2</p>'
-// div.innerHTML = ''
-
-// const par = document.createElement('p')
-// par.textContent = 'qwerty'
-// div.append(par)
-
-
-// const par1 = '<p>qwerty</p>'
-//  div.innerHTML = par1
-// div.insertAdjacentHTML('beforebegin', par1)
-
-// const box = document.createElement('div')
-
-// box.insertAdjacentHTML('beforeend', '<p>1</p>')
-
-// document.body.append(box)
-
-// const array = [{a: 1}, {a: 2}, {a: 3}]
-
-// const markup = array.map(({a}) => `<li>${a}</li>`).join('')
-
-// console.log(markup)
-
-// const list = document.querySelector('.list')
-
-// list.insertAdjacentHTML('afterbegin', markup)
-
-
-// FORBIDDEN !!!!!
-
-// const list = document.querySelector('.list')
-
-// array.forEach(({a}) => list.insertAdjacentHTML('beforeend', `<li>${a}</li>`))
+const link = document.querySelector('a')
 
 
 
 
+link.addEventListener('click', onLink)
 
-// const elements = array.map(({a}) => {
-//     const li = document.createElement('li')
-//     li.textContent = a
-//     return li
+
+
+function onLink(e) {
+    e.preventDefault()
+    console.log(this) 
+}
+
+// document.addEventListener("keydown", event => {
+//     console.log(event)
+//     console.log("key: ", event.key);
+//     console.log("code: ", event.code);
+//     if(event.code === 'Enter') {
+//         console.log('enter')
+//     }
+//   });
+
+// const form = document.querySelector(".form");
+
+// form.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+
+//   const {
+//     elements: { login, password }
+//   } = event.target;
+
+
+//   if (login.value === "" || password.value === "") {
+//       console.log("Please fill in all the fields!");
+//       return
+//   }
+
+//   console.log(`Login: ${login.value}, Password: ${password.value}`);
+//   event.target.reset();
+// }
+
+// const input = document.querySelector('input')
+
+// input.addEventListener('input', (e) => {
+//     console.log(e.target.value)
 // })
-
-// console.log(elements)
-
-// list.append(...elements)
-
-
-
-// FORBIDDEN !!!!!
-
-// const elements = array.forEach(({a}) => {
-//     const li = document.createElement('li')
-//     li.textContent = a
-//     // list.appendChild(li)
-//     list.append(li)
-// })
+// const mango = {
+//     username: "Mango",
+//     showUsername() {
+//       console.log(this);
+//       console.log(`My username is: ${this.username}`);
+//     },
+//   };
+  
+//   const btn = document.querySelector(".btn");
+  
+// //   ✅ Працює
+// //   mango.showUsername();
+  
+//   // ❌ this буде посилатися на button, якщо використовувати showUsername як callback
+// //   btn.addEventListener("click", mango.showUsername); // не працює
+  
+//   // ✅ Не забувайте прив'язувати контекст методів об'єкта
+//   btn.addEventListener("click", mango.showUsername.bind(mango));
 
