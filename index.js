d8704f54747a17f9efdcf29691f1e1ea125059a6
Alona Harnyk <alyonagarnyk@gmail.com>
2023-01-30 13:25:07 +0200

    30.01

--- index.js
+++ index.js
@@ -1,175 +1,24 @@
-// const customer = {
-//   firstName: "Jacob",
-//   lastName: "Mercer",
-//   getFullName() {
-//     console.log(this);
-//     return `${this.firstName} ${this.lastName}`;
-//   },
-// };
-
-// console.log(customer.getFullName());
-
-// const customer1 = {
-//   firstName: "Jacob",
-//   lastName: "Mercer",
-//   getFullName: () => {
-//     console.log(this);
-//     return `${this.firstName} ${this.lastName}`;
-//   },
-// };
-
-// console.log(customer1.getFullName());
-
-// const hotel = {
-//     username: "Resort hotel",
-//     showThis() {
-//       const foo = () => {
-//         // Стрілки запам'ятовують контекст під час оголошення
-//         // з батьківської області видимості
-//         console.log("this in foo: ", this);
-//       };
-  
-//       foo();
-//     //   console.log("this in showThis: ", this);
-//     },
-//   };
-  
-//   hotel.showThis();
-//   // this in foo: {username: 'Resort hotel', showThis: ƒ}
-//   // this in showThis: {username: 'Resort hotel',showThis: ƒ}
 
 
+// const a = (b, c, foo) => {
+//     foo(b, c)
+// }
+
+// export {a, array}
 
 
-// const inventory = {
-//   items: ['Knife', 'Gas mask'],
-//   add(itemName) {
-//     console.log(`Adding ${itemName} to inventory`);
+// const a = 10
 
-//     this.items.push(itemName);
-//   },
-//   remove(itemName) {
-//     console.log(`Removing ${itemName} from inventory`);
+// export const b = () => {
+//     console.log(a)
+// }
 
-//     this.items = this.items.filter(item => item !== itemName);
-//   },
-// };
+// export const c = 100
 
-// const invokeInventoryAction = function(itemName, action) {
-//   console.log(`Invoking action on ${itemName}`);
-//   action(itemName);
-// };
+// export const obj = {q: 90, p: 70, x: 6869}
 
-// invokeInventoryAction('Medkit', inventory.add);
-// // Invoking action on Medkit
-// // Adding Medkit to inventory
+import bkjgbuigb from './script.js'
 
-// // console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+import {a} from './script.js'
 
-// invokeInventoryAction('Gas mask', inventory.remove);
-// // Invoking action on Gas mask
-// // Removing Gas mask from inventory
-
-// // console.log(inventory.items); // ['Knife', 'Medkit']
-
-// // BIND
-
-// const inventory = {
-//   items: ['Knife', 'Gas mask'],
-//   add(itemName) {
-//     console.log(`Adding ${itemName} to inventory`);
-
-//     this.items.push(itemName);
-//   },
-//   remove(itemName) {
-//     console.log(`Removing ${itemName} from inventory`);
-
-//     this.items = this.items.filter(item => item !== itemName);
-//   },
-// };
-
-// const invokeInventoryAction = function (itemName, action) {
-//   console.log(`Invoking action on ${itemName}`);
-//   action(itemName);
-// };
-
-// invokeInventoryAction('Medkit', inventory.add.bind(inventory));
-
-
-// // Invoking action on Medkit
-// // Adding Medkit to inventory
-
-// // console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
-
-// invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
-// // // Invoking action on Gas mask
-// // // Removing Gas mask from inventory
-
-// // // console.log(inventory.items); // ['Knife', 'Medkit']
-
-
-// // CALL
-// const inventory = {
-//   items: ['Knife', 'Gas mask'],
-//   add(itemName) {
-//     console.log(`Adding ${itemName} to inventory`);
-
-//     this.items.push(itemName);
-//   },
-//   remove(itemName) {
-//     console.log(`Removing ${itemName} from inventory`);
-
-//     this.items = this.items.filter(item => item !== itemName);
-//   },
-// };
-
-// const invokeInventoryAction = function (itemName, action) {
-//   console.log(this)
-//   console.log(`Invoking action on ${itemName}`);
-//   action.call(this, itemName);
-// };
-
-// invokeInventoryAction.call(inventory, 'Medkit', inventory.add);
-// // Invoking action on Medkit
-// // Adding Medkit to inventory
-
-// // console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
-
-// invokeInventoryAction.call(inventory, 'Gas mask', inventory.remove);
-// // Invoking action on Gas mask
-// // Removing Gas mask from inventory
-
-// // console.log(inventory.items); // ['Knife', 'Medkit']
-
-// APPLY
-const inventory = {
-  items: ['Knife', 'Gas mask'],
-  add(itemName) {
-    console.log(`Adding ${itemName} to inventory`);
-
-    this.items.push(itemName);
-  },
-  remove(itemName) {
-    console.log(`Removing ${itemName} from inventory`);
-
-    this.items = this.items.filter(item => item !== itemName);
-  },
-};
-
-const invokeInventoryAction = function (itemName, action) {
-  console.log(this)
-  console.log(`Invoking action on ${itemName}`);
-  action.apply(this, [itemName]);
-};
-
-invokeInventoryAction.apply(inventory, ['Medkit', inventory.add]);
-// Invoking action on Medkit
-// Adding Medkit to inventory
-
-// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
-
-invokeInventoryAction.apply(inventory, ['Gas mask', inventory.remove]);
-// Invoking action on Gas mask
-// Removing Gas mask from inventory
-
-// console.log(inventory.items); // ['Knife', 'Medkit']
+console.log(bkjgbuigb)
---
+++ script.js
@@ -0,0 +1,34 @@
+// import {a, array} from './index.js'
+
+
+// const foo = (f, b) => f + b
+
+// console.log(a(5, 70, foo))
+// console.log(array)
+
+
+// import {obj, c, b} from './index.js'
+
+// const {p, y} = obj
+
+// b()
+
+// const a = 809
+
+// console.log(a)
+
+// import * as data from "./index.js";
+
+// const {b} = data
+
+// console.log(b)
+
+
+const b = 40
+const c = () => a + b
+
+export const a = 10
+
+export default {
+    b, c
+}
